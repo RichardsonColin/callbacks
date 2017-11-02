245b7b6d73b8b3f418f6df5f306afd40d5e3c078
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-02 22:33:04 +0000

    output matches the example

--- loaded-die.js
+++ loaded-die.js
@@ -1,9 +1,14 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
   /* your code here */
-
+  var storeNumber = 0;
+  var increment = 0;
   return function() {
     /* your code here */
+    for(var i = increment; i < list.length; i++) {
+      increment = i + 1;
+      return storeNumber = list[i];
+    }
   }
 }
 
